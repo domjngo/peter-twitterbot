8484da486dbd8147bb9f5ef7cc96824a166b497f
Chris Bishop <domingobishop@gmail.com>
2018-09-05 16:44:15 +0100

    output data and analyse

---
+++ analyse-data.py
@@ -0,0 +1,16 @@
+import csv
+from collections import Counter
+
+
+with open('data/newdata.csv', 'r') as data_file:
+    data = csv.reader(data_file, delimiter=',')
+
+    labels = [row[3] for row in data]
+
+    for (k, v) in Counter(labels).items():
+        with open('data/data.csv', 'a') as file:
+            writer = csv.writer(file)
+            row_data = [k, v]
+            writer.writerow(row_data)
+            file.close()
+        print("%s, %d" % (k, v))
--- process-data.py
+++ process-data.py
@@ -1,21 +1,42 @@
 import csv
-import requests
-from bs4 import BeautifulSoup
+
+
+def format_label(label):
+    label = label.replace('-', ' ')
+    label = label.strip()
+
+    return label
+
 
 with open('data/chatdata.csv', 'r', newline='', encoding='mac_roman') as data_file:
     data = csv.reader(data_file, delimiter=',')
 
     for row in data:
-        reply = row[1]
-        if 'contact-us' in reply:
-            label = 'General enquiry'
-        else:
-            url = 'http://www.nationalarchives.gov'+reply
-            print(url)
-            get_content = requests.get(url)
-            get_text = get_content.text
-            soup = BeautifulSoup(get_text, 'html.parser')
-            label = soup.find_next('h1')
+        if len(row[0]) > 5:
+            reply = row[1]
+            if 'contact-us' in reply:
+                label = 'General enquiry'
+            elif '#find-a-research-guide' in reply:
+                label = 'General research guide enquiry'
+            elif 'research-guides' in reply:
+                bits = reply.split('/')
+                for i,bit in enumerate(bits):
+                    if bit == 'research-guides':
+                        label = bits[i+1]
+            elif 'discovery-help' in reply:
+                bits = reply.split('/')
+                for i, bit in enumerate(bits):
+                    if bit == 'discovery-help':
+                        label = bits[i + 1] + ' ' + bits[i + 2]
+            else:
+                label = 'General research help enquiry'
 
-        print(label)
+            label = label.capitalize()
+            label = format_label(label)
+            with open('data/newdata.csv', 'a') as new_csv:
+                writer = csv.writer(new_csv)
+                row_data = [format_label(row[0]), row[1].strip(), format_label(row[2]), label]
+                writer.writerow(row_data)
+            new_csv.close()
 
+data_file.close()
