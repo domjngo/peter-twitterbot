5287f49075ccb3a7db5467bbac391fa62a4029ef
domingobishop <domingobishop@gmail.com>
2019-02-04 12:31:14 +0000

    change dir

--- analyse-data.py
+++ analyse-data.py
@@ -2,13 +2,13 @@ import csv
 from collections import Counter
 
 
-with open('data/newdata.csv', 'r') as data_file:
+with open('data/peter-bot/newdata.csv', 'r') as data_file:
     data = csv.reader(data_file, delimiter=',')
 
     labels = [row[3] for row in data]
 
     for (k, v) in Counter(labels).items():
-        with open('data/labeldata.csv', 'a') as file:
+        with open('data/peter-bot/labeldata.csv', 'a') as file:
             writer = csv.writer(file)
             row_data = [k, v]
             writer.writerow(row_data)
